14984c8dc0210ed91faffdad9cfb92b31537dd36
saranyaa25 <saranyasowmy2596>
2017-04-10 23:29:58 +0530

    this is my third commit

---
+++ Static.java
@@ -0,0 +1,16 @@
+package hell;
+
+public class Static
+{     
+	static int j=90; //global    
+	static int i=89;     
+	public static void main (String[] args) 
+	{
+		System.out.println(i);
+		System.out.println(j);
+		}
+	 
+	}
+
+
+
